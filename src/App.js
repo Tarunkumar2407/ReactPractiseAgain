842ed2594cf481dc6bb4652739a6c5e8a3293092
Tarun Kumar <tarunkumar2407@gmail.com>
2023-10-20 10:07:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import './App.css';
-import Parent from './components/PropDrilling/Parent';
-// import Variables from './components/Hook/Variables';
-// import UseEffectHook from './components/Hook/useEffect.js/UseEffectHook';
-// import ListRendering from './components/ListRendering';
-// import Parent from './components/liftingStateUp/Parent';
-// import PassFunction from './components/Props/PassFunction';
-
-
 
 function App() {
   return (
     <div className="App">
-    {/* <PassFunction /> */}
-    {/* <Variables /> */}
-    {/* <UseEffectHook /> */}
-    {/* <ListRendering /> */}
-    {/* < Parent /> */}
-    <Parent />
+    <h1>Hello World</h1>
     </div>
   );
 }
