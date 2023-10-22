905a613cb6f85f85b0aaf862def835fd373268fd
Tarun Kumar <tarunkumar2407@gmail.com>
2023-10-22 19:51:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from "react"
 import './App.css';
-import CakeContainer from "./Redux/Cake/CakeContainer";
-import { Provider } from "react-redux";
-import store from "./Redux/store";
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Home from "./Router/Pages/Home";
+import About from "./Router/Pages/About";
+import Page404 from "./Router/Pages/Page404";
+import { Navigate } from 'react-router-dom';
+import User from './Router/Pages/User';
 
 function App() {
 
   return (
     <div className="App">
-    <Provider store = {store}>
-         <CakeContainer />
-    </Provider>
+    <BrowserRouter>
+    <Link to="/">Home</Link><br />
+    <Link to="/about">About</Link><br />
+    <Link to="/contact">Contact</Link><br />
+    <Link to="/user/Anil">Anil</Link><br />
+    <Link to="/user/Peter">Peter</Link><br />
+      <Routes>
+        <Route path="/"  element={<Home />}></Route>
+        <Route path="/about"  element={<About />}></Route>
+        {/* <Route path="/*"  element={<Page404 />}></Route> */}
+        <Route path="/user/:name"  element={<User />}></Route>
+        <Route path="/*"  element={<Navigate to="/" />}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
 
   );
